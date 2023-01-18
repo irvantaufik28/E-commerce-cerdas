18f13cf77ab5b8edc4c0ed73548402c9ec70177b
irvan taufik <irvantaufik28@gmail.com>
2023-01-18 16:46:13 +0700

    nodemailer otp repo

--- src/helpers/functions.js
+++ src/helpers/functions.js
@@ -15,4 +15,11 @@ module.exports = {
     }
     return phone;
   },
+  generateRandomNumber(len) {
+    let randStr = ''
+    for (let i = 0; i < len; i++) {
+        randStr += Math.floor(Math.random() * 10)
+    }
+    return randStr
+}
 };
---
+++ src/models/otp.js
@@ -0,0 +1,29 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class otp extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  otp.init({
+    email: DataTypes.STRING,
+    otp_type: DataTypes.STRING,
+    otp_code: DataTypes.STRING,
+    expired_at: DataTypes.DATE
+  }, {
+    sequelize,
+    modelName: 'otp',
+    tableName: "otp",
+    createdAt: "created_at",
+    updatedAt: "updated_at",
+  });
+  return otp;
+};
