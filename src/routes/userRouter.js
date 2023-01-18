9dfa8722113719e41812349510026cc74f5af361
irvan taufik <irvantaufik28@gmail.com>
2023-01-18 23:25:17 +0700

    fix router

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,15 +1,16 @@
-const router = require("express").Router();
-const userController = require("../controller/userController");
+const router = require("express").Router()
+const userController = require("../controller/userController")
 
-const handleUpload = require("../helpers/mediaHandler");
+const handleUpload = require("../helpers/mediaHandler")
 
 const authorized = require("../middlerware/authorization")
 
-router.get("/api/v1/user",authorized, userController.profile);
-router.get("/api/v1/user/:id",authorized, userController.geUserByid);
-router.get("/api/v1/all/user",authorized, userController.getAllUsers);
+router.get("/api/v1/auth/user", authorized, userController.profile)
+router.post("/api/v1/auth/reset-password", userController.forgetPassword)
+
+router.get("/api/v1/user/:id", authorized, userController.geUserByid)
+router.get("/api/v1/user", authorized, userController.getAllUsers)
 router.put("/api/v1/user", authorized, handleUpload.upload.single("image"), userController.update)
 router.delete("/api/v1/user", authorized, userController.deleteAccount)
 router.post("/api/v1/user", authorized, handleUpload.upload.single("image"), userController.create)
-router.post("/api/v1/user/reset-password", userController.forgetPassword)
-module.exports = router;
+module.exports = router
