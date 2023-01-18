67751c3cdd011d97626b0119777f1015a8d75b79
irvan taufik <irvantaufik28@gmail.com>
2023-01-18 12:26:06 +0700

    basic usecase products

---
+++ src/use_case/productsUseCase.js
@@ -0,0 +1,41 @@
+class ProductsUseCase {
+  constructor(productsRepository) {
+    this._productRepository = productsRepository;
+  }
+
+  async getAll() {
+    const products = await this._productsRepository.getAll();
+    return products;
+  }
+
+  async getByid() {
+    const products = await this._productsRepository.getByid();
+    if (!products) {
+      throw { status: 404, message: "Products not found" };
+    }
+    return products;
+  }
+
+  async update(id, products) {
+    const products = await this._productsRepository.getByid(id);
+    if (!products) {
+      throw { status: 404, message: "Products not found" };
+    }
+    await this._productRepository._update(id, products);
+    const newProducts = await this._productRepository.getByid(id);
+
+    return newProducts;
+  }
+
+  async delete(id) {
+    const products = await this._productsRepository.getByid(id);
+    if (!products) {
+      throw { status: 404, message: "Products not found" };
+    }
+    await this._productRepository.delete(id);
+
+    return;
+  }
+}
+
+module.exports = ProductsUseCase;
