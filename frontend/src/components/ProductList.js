9b24fea595f85540fb086797acd257c98d559c5d
irvan taufik <irvantaufik28@gmail.com>
2023-02-01 10:17:03 +0700

    add frontend reacjs Get all Proudcts

---
+++ frontend/src/components/ProductList.js
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+const ProductList = () => {
+    const [products, setProducts] = useState([])
+
+    useEffect(()=>{
+        getProducts()
+    },[])
+
+    const getProducts = async () => {
+        const response = await axios.get('http://localhost:3000/api/v1/product')
+        setProducts(response.data.products.data)
+    }
+  return (
+    <div className="columns mt-5 is-centered">
+      <div className="column is-half">
+        <table className="table is-striped is-fullwidth">
+          <thead>
+            <tr>
+              <th>No</th>
+              <th>Seller</th>
+              <th>Price</th>
+              <th>Descripition</th>
+              <th>image</th>
+              <th>action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {products.map((product, index)=>(
+
+            <tr key={product.id}>
+              <td>{index + 1}</td>
+              <td>{product.user_id}</td>
+              <td>{product.name_product}</td>
+              <td>{product.descripition}</td>
+              <td>  <img src={product.image} alt="icons" /></td>
+              <td>
+                <button className="button is-small is-info">edit</button> 
+                <button className="button is-small is-danger">delete</button>
+              </td>
+            </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default ProductList;
